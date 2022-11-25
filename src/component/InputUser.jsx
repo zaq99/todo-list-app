f49479840989606dae51f1e0927301fcadf6f17b
zaq <85076901+zaq99@users.noreply.github.com>
2022-11-25 23:03:58 +0700

    add component and install tailwind Css

---
+++ src/component/InputUser.jsx
@@ -0,0 +1,9 @@
+const InputUser = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Halo Saya Dari Input User</h1>
+        </>
+    )
+}
+
+export default InputUser
